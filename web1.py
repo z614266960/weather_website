589d24d573d699fa5fe37a6cb612c509a173ccfc
zrj <614266960@qq.com>
2020-11-27 16:11:29 +0800

    lstm

---
+++ web1.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 23 14:06:13 2020
+
+@author: 61426
+"""
+
+import os
+from flask import Flask, flash, request, redirect, url_for, escape,jsonify,make_response
+from flask_cors import CORS
+from flask import render_template
+
+from build_model import lstm_model
+app = Flask(__name__)
+# 设置跨域
+CORS(app, resources=r'/*')
+# app.debug = True
+
+
+
+# 进入建立lstm页面
+@app.route('/build/lstm/view')
+def build_lstm_view():
+    return render_template('build_lstm_view.html')
+
+
+# 接收lstm页面
+@app.route('/build_lstm_data',methods=['POST'])
+def build_lstm():
+    id = request.form['id']
+    time = request.form['time']
+    type = request.form['type']
+    dir = request.form['dir']
+    
+    # TODO 处理数据
+    data = []
+    
+    lstm_model.build_lstm(id,time)
+    return 'ok'
+
+if __name__ == '__main__':
+    app.run(threaded=True)
